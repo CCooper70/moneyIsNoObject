1a6c897d090c7d809bf43c93554cddf41a55fc59
christy cooper betty <ccooper.prsvr@gmail.com>
2023-12-19 21:59:50 -0600

    not right

--- main.js
+++ main.js
@@ -1,8 +1,51 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+// function getFirstName(obj){
+//     return obj.firstName
+// }
+// function isAdmin(user){
+//      if(user.isAdmin){
+//         return true
+//      }else {
+//         return false
+//      }
+// }
+function isAdmin(user){
+if(user === "firstName" && user === "lastName" === admin){
 
+}
+return admin
+}
 
+function getEmail(user){
+//     let firstName = user.first[0];
+//     let lastName = user.last;
+//     let email = firstName + lastName + ".prsvr@gmail.com" 
+return email[user.firstName[0].toLowerCase() + user.lastName.toLowerCase] + ".prsvr@gmail.com"
+}
+function getPlaylistLength(playlist){
+    return playlist["songs"].length 
+}
+
+function getHardestHomework(arr){
+    for(let i = 0; i < getHardestHomework.length; i++){
+          console.log(getHardestHomework[i])
+    }
+}
+
+function createPhonebook(){ 
+        for (i = 0; i < phoneBook.length; i++) {
+            if (phoneBook[i].number.equals(num)) {
+                if (phoneBook[i].firstName.equals(fName) || fName.equals("")) {
+                    return phoneBook[i];
+                }
+            }
+        }
+    
+        return null;
+    
+    }
 
 
 
